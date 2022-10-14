3db2d6c1568028870ab1d20e95fcbf3897e3b7a9
NICKsensei <80743409+NICKsensei@users.noreply.github.com>
2022-10-14 19:08:53 +0600

    c

--- index.js
+++ index.js
@@ -2,19 +2,36 @@ let firstCard = 10;
 let secondCard = 11;
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
-// 1. Create a variable called isAlive and assign it to true
 let isAlive = true;
-// 2. Flip its value to false in the appropriate code block
-if (sum <= 20) {
-  console.log("Do you want to draw a new card? 🙂");
-} else if (sum === 21) {
-  console.log("Wohoo! You've got Blackjack! 🥳");
-  hasBlackJack = true;
-} else {
-  console.log("You're out of the game! 😭");
-  isAlive = false;
+let message = "";
+
+// 1. Store the message-el paragraph in a variable called messageEl
+let messageEl = document.getElementById("message-el");
+// let sumEl = document.getElementById("sum-el");
+let sumEl = document.querySelector("#sum-el");
+let cardsEl = document.querySelector("#cards-el");
+
+function startGame() {
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "You've got Blackjack!";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game!";
+    isAlive = false;
+  }
+  // 2. Display the message in the messageEl using messageEl.textContent
+  messageEl.textContent = message;
+  sumEl.textContent = "Sum: " + sum;
+  cardsEl.textContent = "Cards: " + firstCard + " + " + secondCard;
 }
 
-// 3. Log it out to check that you're doing it right
-console.log(isAlive);
-console.log(hasBlackJack);
+function newCard() {
+  messageEl.textContent = "Drawing a new card from the deck!";
+  // 1. Create a card variable, and hard code its value to a number (2-11)
+
+  // 2. Add the new card to the sum variable
+
+  // 3. Call startGame()
+}
