5eb859f10c095f4f307c72eced85a77a4355ddbd
NICKsensei <80743409+NICKsensei@users.noreply.github.com>
2022-10-14 20:11:42 +0600

    c

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-let firstCard = 10;
-let secondCard = 11;
+let firstCard = 10,
+  secondCard = 5,
+  thirdCard = 3;
+let cards = [firstCard, secondCard, thirdCard];
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
-
-// 1. Store the message-el paragraph in a variable called messageEl
 let messageEl = document.getElementById("message-el");
-// let sumEl = document.getElementById("sum-el");
 let sumEl = document.querySelector("#sum-el");
 let cardsEl = document.querySelector("#cards-el");
 
 function startGame() {
+  renderGame();
+}
+
+function renderGame() {
   if (sum <= 20) {
     message = "Do you want to draw a new card?";
   } else if (sum === 21) {
@@ -21,17 +24,13 @@ function startGame() {
     message = "You're out of the game!";
     isAlive = false;
   }
-  // 2. Display the message in the messageEl using messageEl.textContent
   messageEl.textContent = message;
   sumEl.textContent = "Sum: " + sum;
-  cardsEl.textContent = "Cards: " + firstCard + " + " + secondCard;
+  cardsEl.textContent = "Cards: " + cards[0] + " + " + cards[1];
 }
 
 function newCard() {
   messageEl.textContent = "Drawing a new card from the deck!";
-  // 1. Create a card variable, and hard code its value to a number (2-11)
-
-  // 2. Add the new card to the sum variable
-
-  // 3. Call startGame()
+  sum += thirdCard;
+  startGame();
 }
